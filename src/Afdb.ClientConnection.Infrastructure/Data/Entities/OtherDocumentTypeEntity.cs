f76609bd6d2a721330e9bcce37a33954fa749665
dmatie <111058516+dmatie@users.noreply.github.com>
2025-12-14 00:00:42 +0000

    Updated OtherDocumentTypeEntity.cs

--- src/Afdb.ClientConnection.Infrastructure/Data/Entities/OtherDocumentTypeEntity.cs
+++ src/Afdb.ClientConnection.Infrastructure/Data/Entities/OtherDocumentTypeEntity.cs
@@ -4,6 +4,7 @@ public class OtherDocumentTypeEntity : BaseEntityConfiguration
 {
     public string Name { get; set; } = string.Empty;
     public string NameFr { get; set; } = string.Empty;
+    public bool IsActive { get; set; } 
 
     public ICollection<OtherDocumentEntity> OtherDocuments { get; set; } = new List<OtherDocumentEntity>();
 }
